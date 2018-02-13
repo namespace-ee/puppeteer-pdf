ca94efa4963cba035dd3d0728bc46fd6fbd5eecf
Madis Väin <madisvain@gmail.com>
2018-02-13 14:03:31 +0200

    working commander with options loop

--- puppeteer-pdf.js
+++ puppeteer-pdf.js
@@ -8,40 +8,50 @@ const puppeteer = require('puppeteer');
 
 cli
   .version('0.1.0')
-  .option('-u, --url', 'URL to render into PDF.')
-  .option('-p, --path', 'The file path to save the PDF to.')
-  .option('-s, --scale', 'Scale of the webpage rendering. Defaults to 1.')
-  .option('-dhf, --displayHeaderFooter', 'Display header and footer. Defaults to false.')
-  .option('-ht, --headerTemplate', 'HTML template for the print header.')
-  .option('-ft, --footerTemplate', 'HTML template for the print footer.')
-  .option('-ht, --printBackground', 'Print background graphics. Defaults to false.')
-  .option('-ft, --landscape', 'Paper orientation. Defaults to false.')
-  .option('-pr, --pageRanges', 'Paper ranges to print, e.g., \'1-5, 8, 11-13\'. Defaults to the empty string, which means print all pages.')
-  .option('-f, --format', 'Paper orientation. Defaults to false.')
-  .option('-w, --width', 'Paper orientation. Defaults to false.')
-  .option('-h, --height', 'Paper orientation. Defaults to false.')
-  .option('-mt, --margintTop', 'Paper margin top, defaults to none.')
-  .option('-mr, --margintRight', 'Paper margin right, defaults to none.')
-  .option('-mb, --margintBottom', 'Paper margin bottom, defaults to none.')
-  .option('-ml, --margintLeft', 'Paper margin left, defaults to none.')
+  .option('-p, --path <path>', 'The file path to save the PDF to.')
+  .option('-s, --scale [scale]', 'Scale of the webpage rendering.', parseInt, 1)
+  .option('-dhf, --displayHeaderFooter', 'Display header and footer.', false)
+  .option('-ht, --headerTemplate [template]', 'HTML template for the print header.')
+  .option('-ft, --footerTemplate [template]', 'HTML template for the print footer.')
+  .option('-ht, --printBackground', 'Print background graphics.', false)
+  .option('-ft, --landscape', 'Paper orientation.', false)
+  .option('-pr, --pageRanges <range>', 'Paper ranges to print, e.g., \'1-5, 8, 11-13\'. Defaults to the empty string, which means print all pages.')
+  .option('-f, --format [format]', 'Paper format. If set, takes priority over width or height options. Defaults to \'Letter\'.', 'Letter')
+  .option('-w, --width [width]', 'Paper width, accepts values labeled with units.')
+  .option('-h, --heigh [height]', 'Paper height, accepts values labeled with units.')
+  .option('-mt, --marginTop [margin]', 'Top margin, accepts values labeled with units.')
+  .option('-mr, --marginRight [margin]', 'Right margin, accepts values labeled with units.')
+  .option('-mb, --marginBottom [margin]', 'Bottom margin, accepts values labeled with units.')
+  .option('-ml, --marginLeft [margin]', 'Left margin, accepts values labeled with units.')
+  .action(function(required, optional) {
+    // TODO: Implement required arguments validation
+  })
   .parse(process.argv);
 
 (async () => {
-  let options = _.omit(args, ['_', 'marginTop', 'marginRight', 'marginBottom', 'marginLeft'])
+  let options = {}
 
-  const margin = _.pickBy({
-    top: _.get(args, 'marginTop', null),
-    right: _.get(args, 'marginRight', null),
-    bottom: _.get(args, 'marginBottom', null),
-    left: _.get(args, 'marginLeft', null)
-  }, _.identity)
-  if (!_.isEmpty(margin)) {
-    options['margin'] = margin
-  }
+  // Loop through options
+  _.each(cli.options, function(option) {
+    const optionName = option.name()
+    if (!_.isNil(cli[optionName]) && !['version'].includes(optionName)) {
+      const optionValue = cli[optionName]
+
+      if (_.startsWith(optionName, 'margin')) {
+        // Margins need to be combined into an object
+        _.set(options, ['margin', optionName.replace('margin', '').toLowerCase()], optionValue)
+      } else {
+        _.set(options, optionName, optionValue)
+      }
+    }
+  })
+  
+  // Get URL from first argument
+  const url = _.first(cli.args)
 
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto(cli.url, {
+  await page.goto(url, {
     waitUntil: 'networkidle2'
   })
   await page.pdf(options)
