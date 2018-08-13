3c260e6b4378b3dc42d7b97fce7c72aa16944e78
Madis Väin <madisvain@gmail.com>
2018-08-13 13:00:16 +0300

    add license, fix readme and update with waitUntil option

--- README.md
+++ README.md
@@ -1,12 +1,67 @@
 # Puppeteer PDF CLI
 
-Created PDF from the command line with Puppeteer.
+HTML to PDF from the command line with Puppeteer.
 
-### Testing
+[![npm downloads](https://img.shields.io/npm/dt/puppeteer-pdf.svg)](https://npmjs.org/package/puppeteer-pdf)
+[![npm license](https://img.shields.io/npm/l/puppeteer-pdf.svg)](https://npmjs.org/package/puppeteer-pdf)
+
+
+## Help
+```
+puppeteer-pdf --help
+  Usage: puppeteer-pdf [options]
+
+
+  Options:
+
+    -V, --version                     output the version number
+    -p, --path <path>                 The file path to save the PDF to.
+    -s, --scale [scale]               Scale of the webpage rendering. (default: 1)
+    -dhf, --displayHeaderFooter       Display header and footer.
+    -ht, --headerTemplate [template]  HTML template for the print header.
+    -ft, --footerTemplate [template]  HTML template for the print footer.
+    -pb, --printBackground            Print background graphics.
+    -l, --landscape                   Paper orientation.
+    -pr, --pageRanges <range>         Paper ranges to print, e.g., '1-5, 8, 11-13'. Defaults to the empty string, which means print all pages.
+    -f, --format [format]             Paper format. If set, takes priority over width or height options. Defaults to 'Letter'. (default: Letter)
+    -w, --width [width]               Paper width, accepts values labeled with units.
+    -h, --heigh [height]              Paper height, accepts values labeled with units.
+    -mt, --marginTop [margin]         Top margin, accepts values labeled with units.
+    -mr, --marginRight [margin]       Right margin, accepts values labeled with units.
+    -mb, --marginBottom [margin]      Bottom margin, accepts values labeled with units.
+    -ml, --marginLeft [margin]        Left margin, accepts values labeled with units.
+    -d, --debug                       Output Puppeteer PDF options
+    -wu, --waitUntil [choice]         waitUntil accepts choices load, domcontentloaded, networkidle0, networkidle2. Defaults to 'networkidle2'. (default: networkidle2)
+    -h, --help                        output usage information
+```
+
+## Examples
 ```shell
-puppeteer-pdf tests/test.html --path g.pdf --landscape --headerTemplate=file:///Users/konstruktor/Documents/puppeteer-pdf/tests/header.html --debug --marginTop 500px --marginLeft 200px --scale 2 --displayHeaderFooter
+puppeteer-pdf tests/test.html \
+  --path demo.pdf \
+  --landscape \
+  --debug \
+  --waitUntil networkidle0 \
+  --displayHeaderFooter
 ```
 
 ```shell
-puppeteer-pdf tests/test.html --path g.pdf --landscape --headerTemplate='<table style="width: 100%; margin-left: 5px; margin-right: 5px; margin-bottom: 0px;"> <tr> <td class="section" style="text-align:left"> <div style="font-size: 10px;"> <p>Report Name</p> <p>Some Text</p></div> </td> <td style="text-align:right"> <div style="font-size: 10px;"> <p>Start - End</p> </div> </td> </tr> </table>' --debug --marginTop 500px --marginLeft200px --scale 2 --displayHeaderFooter
+puppeteer-pdf tests/test.html \
+  --path demo-file-header.pdf \
+  --landscape \
+  --headerTemplate=file://tests/header.html \
+  --debug \
+  --marginTop 200px \
+  --scale 2 \
+  --displayHeaderFooter
+```
+
+```shell
+puppeteer-pdf tests/test.html \
+  --path demo-inline-header.pdf \
+  --landscape \
+  --headerTemplate='<table style="width: 100%; margin-left: 5px; margin-right: 5px; margin-bottom: 0px;"> <tr> <td class="section" style="text-align:left"> <div style="font-size: 10px;"> <p>Report Name</p> <p>Some Text</p></div> </td> <td style="text-align:right"> <div style="font-size: 10px;"> <p>Start - End</p> </div> </td> </tr> </table>' \
+  --debug \
+  --marginTop 200px \
+  --displayHeaderFooter
 ```
--- puppeteer-pdf.js
+++ puppeteer-pdf.js
@@ -1,74 +1,121 @@
 #!/usr/bin/env node
 
-const _ = require('lodash');
-const cli = require('commander');
-const fileUrl = require('file-url');
-const fs = require('fs');
-const isUrl = require('is-url');
-const puppeteer = require('puppeteer');
-
+const _ = require("lodash");
+const cli = require("commander");
+const fileUrl = require("file-url");
+const fs = require("fs");
+const isUrl = require("is-url");
+const puppeteer = require("puppeteer");
 
 cli
-  .version('1.1.3')
-  .option('-p, --path <path>', 'The file path to save the PDF to.')
-  .option('-s, --scale [scale]', 'Scale of the webpage rendering.', parseFloat, 1)
-  .option('-dhf, --displayHeaderFooter', 'Display header and footer.', false)
-  .option('-ht, --headerTemplate [template]', 'HTML template for the print header.')
-  .option('-ft, --footerTemplate [template]', 'HTML template for the print footer.')
-  .option('-pb, --printBackground', 'Print background graphics.', false)
-  .option('-l, --landscape', 'Paper orientation.', false)
-  .option('-pr, --pageRanges <range>', 'Paper ranges to print, e.g., \'1-5, 8, 11-13\'. Defaults to the empty string, which means print all pages.')
-  .option('-f, --format [format]', 'Paper format. If set, takes priority over width or height options. Defaults to \'Letter\'.', 'Letter')
-  .option('-w, --width [width]', 'Paper width, accepts values labeled with units.')
-  .option('-h, --heigh [height]', 'Paper height, accepts values labeled with units.')
-  .option('-mt, --marginTop [margin]', 'Top margin, accepts values labeled with units.')
-  .option('-mr, --marginRight [margin]', 'Right margin, accepts values labeled with units.')
-  .option('-mb, --marginBottom [margin]', 'Bottom margin, accepts values labeled with units.')
-  .option('-ml, --marginLeft [margin]', 'Left margin, accepts values labeled with units.')
-  .option('-d, --debug', 'Output Puppeteer PDF options')
+  .version("1.2.0")
+  .option("-p, --path <path>", "The file path to save the PDF to.")
+  .option(
+    "-s, --scale [scale]",
+    "Scale of the webpage rendering.",
+    parseFloat,
+    1
+  )
+  .option("-dhf, --displayHeaderFooter", "Display header and footer.", false)
+  .option(
+    "-ht, --headerTemplate [template]",
+    "HTML template for the print header."
+  )
+  .option(
+    "-ft, --footerTemplate [template]",
+    "HTML template for the print footer."
+  )
+  .option("-pb, --printBackground", "Print background graphics.", false)
+  .option("-l, --landscape", "Paper orientation.", false)
+  .option(
+    "-pr, --pageRanges <range>",
+    "Paper ranges to print, e.g., '1-5, 8, 11-13'. Defaults to the empty string, which means print all pages."
+  )
+  .option(
+    "-f, --format [format]",
+    "Paper format. If set, takes priority over width or height options. Defaults to 'Letter'.",
+    "Letter"
+  )
+  .option(
+    "-w, --width [width]",
+    "Paper width, accepts values labeled with units."
+  )
+  .option(
+    "-h, --heigh [height]",
+    "Paper height, accepts values labeled with units."
+  )
+  .option(
+    "-mt, --marginTop [margin]",
+    "Top margin, accepts values labeled with units."
+  )
+  .option(
+    "-mr, --marginRight [margin]",
+    "Right margin, accepts values labeled with units."
+  )
+  .option(
+    "-mb, --marginBottom [margin]",
+    "Bottom margin, accepts values labeled with units."
+  )
+  .option(
+    "-ml, --marginLeft [margin]",
+    "Left margin, accepts values labeled with units."
+  )
+  .option("-d, --debug", "Output Puppeteer PDF options")
+  .option(
+    "-wu, --waitUntil [choice]",
+    "waitUntil accepts choices load, domcontentloaded, networkidle0, networkidle2. Defaults to 'networkidle2'.",
+    "networkidle2"
+  )
   .action(function(required, optional) {
     // TODO: Implement required arguments validation
   })
   .parse(process.argv);
 
 (async () => {
-  let options = {}
+  let options = {};
 
   // Loop through options
   _.each(cli.options, function(option) {
-    const optionName = option.name()
-    if (!_.isNil(cli[optionName]) && !['version'].includes(optionName)) {
-      const optionValue = cli[optionName]
+    const optionName = option.name();
+    if (!_.isNil(cli[optionName]) && !["version"].includes(optionName)) {
+      const optionValue = cli[optionName];
 
-      if (_.startsWith(optionName, 'margin')) {
+      if (_.startsWith(optionName, "margin")) {
         // Margins need to be combined into an object
-        _.set(options, ['margin', optionName.replace('margin', '').toLowerCase()], optionValue)
+        _.set(
+          options,
+          ["margin", optionName.replace("margin", "").toLowerCase()],
+          optionValue
+        );
       } else {
-        _.set(options, optionName, optionValue)
+        _.set(options, optionName, optionValue);
       }
     }
-  })
+  });
 
   // Check if we need to read header or footer templates from files
-  _.each(['headerTemplate', 'footerTemplate'], function(template) {
-    if (_.get(options, template, '').startsWith('file://')) {
-      options[template] = fs.readFileSync(options[template].replace('file://', ''), 'utf-8')
+  _.each(["headerTemplate", "footerTemplate"], function(template) {
+    if (_.get(options, template, "").startsWith("file://")) {
+      options[template] = fs.readFileSync(
+        options[template].replace("file://", ""),
+        "utf-8"
+      );
     }
-  })
+  });
 
-  const browser = await puppeteer.launch({args: ['--no-sandbox']})
-  const page = await browser.newPage()
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+  const page = await browser.newPage();
 
   // Get URL / file path from first argument
-  const location = _.first(cli.args)
+  const location = _.first(cli.args);
   await page.goto(isUrl(location) ? location : fileUrl(location), {
-    waitUntil: 'networkidle2'
-  })
+    waitUntil: _.get(options, "waitUntil", "networkidle2")
+  });
   // Output options if in debug mode
   if (cli.debug) {
-    console.log(options)
+    console.log(options);
   }
-  await page.pdf(options)
+  await page.pdf(options);
 
-  await browser.close()
+  await browser.close();
 })();
